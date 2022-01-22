8316ac9c0e0b695c3d2df49beb13177a636d9d62
Francisco Sousa <franciscoeds1987@gmail.com>
2022-01-22 09:41:42 -0300

    Cluster stack creation with AWS CDK

--- curso_aws_cdk/src/main/java/com/myorg/CursoAwsCdkApp.java
+++ curso_aws_cdk/src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -1,16 +1,15 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.Environment;
-import software.amazon.awscdk.StackProps;
-
-import java.util.Arrays;
 
 public class CursoAwsCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new VpcStack(app, "Vpc-github");
+        VpcStack vpcStack = new VpcStack(app, "Vpc-github");
+
+        ClusterStack clusterStack = new ClusterStack(app, "Cluster-github", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
 
         app.synth();
     }
--- curso_aws_cdk/src/main/java/com/myorg/VpcStack.java
+++ curso_aws_cdk/src/main/java/com/myorg/VpcStack.java
@@ -6,6 +6,9 @@ import software.amazon.awscdk.services.ec2.Vpc;
 import software.constructs.Construct;
 
 public class VpcStack extends Stack {
+
+    private Vpc vpc;
+
     public VpcStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -13,8 +16,12 @@ public class VpcStack extends Stack {
     public VpcStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        Vpc.Builder.create(this, "Vpc01-github")
+        vpc = Vpc.Builder.create(this, "Vpc01-github")
                 .maxAzs(3)
                 .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
     }
 }
