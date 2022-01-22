282a878a9f04ad9b56665d24aeab02e1d1f1d932
Francisco Sousa <franciscoeds1987@gmail.com>
2022-01-22 10:34:42 -0300

    Creation of the application load balancer and other resources

---
+++ curso_aws_cdk/src/main/java/com/myorg/Service01Stack.java
@@ -0,0 +1,46 @@
+package com.myorg;
+
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+
+public class Service01Stack extends Stack {
+    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+        this(scope, id, null, cluster);
+    }
+
+    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+        super(scope, id, props);
+
+        ApplicationLoadBalancedFargateService service01 =
+                ApplicationLoadBalancedFargateService.Builder.create(this, "ALB01-github")
+                        .serviceName("service-01-github")
+                        .cluster(cluster)
+                        .cpu(512)
+                        .memoryLimitMiB(1024)
+                        .desiredCount(2)
+                        .listenerPort(8080)
+                        .taskImageOptions(
+                                ApplicationLoadBalancedTaskImageOptions.builder()
+                                        .containerName("aws-project01")
+                                        .image(ContainerImage.fromRegistry("franciscos1987/curso_aws_project01"))
+                                        .containerPort(8080)
+                                        .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                                .logGroup(LogGroup.Builder.create(this, "Service01LogGroup-github")
+                                                        .logGroupName("Service01-github")
+                                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                                        .build())
+                                                .streamPrefix("Service01-github")
+                                                .build()))
+                                        .build())
+                        .build();
+    }
+}
